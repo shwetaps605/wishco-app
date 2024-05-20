b36864d8a4368d9b27f622e176669cd8d686d60b
Roy <shweroy@apac.corpdir.net>
2024-05-20 23:46:13 +0530

    add tanstack query

---
+++ components/JobsComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+const JobsComponent = () => {
+  return (
+    <div>JobsComponent</div>
+  )
+}
+
+export default JobsComponent
