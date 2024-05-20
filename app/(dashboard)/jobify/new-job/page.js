c8a03e42af35cea788c45ca2c5840c94059166a1
Roy <shweroy@apac.corpdir.net>
2024-05-20 23:16:05 +0530

    add page for jobs

---
+++ app/(dashboard)/jobify/new-job/page.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+import AddJobForm from '../../../../components/AddJobForm'
+
+const NewJob = () => {
+    return (
+        <>
+          <AddJobForm />
+        </>
+      )
+}
+
+export default NewJob
