3604a7873a4b6ab7d11f47ba3882197dfafe91ca
Roy <shweroy@apac.corpdir.net>
2024-06-04 14:58:04 +0530

    added avatar and more info to featured review

--- components/CompanyDetails.jsx
+++ components/CompanyDetails.jsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import React from 'react'
+import { FaRegUser } from "react-icons/fa6";
+import * as dayjs from 'dayjs'
+var relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
 
 const CompanyDetails = ({company}) => {
   return (
-    <div>
+    <div className='w-[60%] bg-base-100 px-5 py-3 rounded-xl shadow-lg mt-3'>
         <div className='flex flex-row align-middle items-center mt-2'>
             <Image 
                 src={company?.squareLogo}
@@ -12,21 +16,31 @@ const CompanyDetails = ({company}) => {
                 className='w-30 h-30 rounded-full shadow-lg mr-2'
                 alt={company?.name}
             />
-            <p className='text-md text-secondary'>{company.name}</p>
+            <div>
+            <p className='text-md'>{company.name}</p>
+            <p className='text-sm opacity-40'>{company.sectorName}</p>
+
+
+            </div>
         </div>
         {/* <p>{company.overallRating}</p> */}
         {company?.featuredReview ? 
-        <div className='flex flex-col px-5 py-2 bg-base-200 mt-5'>
-        <h2 className='text-md text-primary'>Featured review</h2>
-        <div className='chat chat-start mt-4'>
-            <div className="chat-header">
-                {company?.featuredReview?.jobTitle} | {company?.featuredReview?.location}
-                <time className='text-xs opacity-50'>{company?.featuredReview?.reviewDateTime}</time>
+        <div className='flex flex-col mt-5'>
+        <h2 className='text-md text-accent'>Featured review</h2>
+        <div className='chat chat-start mt-4 w-full'>
+            <div class="chat-image">
+                <div class="w-12 h-12 bg-base-300 flex items-center justify-center rounded-full">
+                    <FaRegUser/>
+                </div>
+            </div>
+            <div className="chat-header flex flex-row justify-between">
+                <span className='text-xs mr-2'>{company?.featuredReview?.jobTitle} | {company?.featuredReview?.location}</span>
+                <time className='text-xs opacity-50'>{dayjs(company?.featuredReview?.reviewDateTime).fromNow()}</time>
             </div> 
             <div className="chat-bubble mt-2">
                 <p className='text-lg text-slate-100'>{company?.featuredReview?.headline}</p>
-                <span className='text-success text-sm mr-2'>Pros: {company?.featuredReview?.pros}</span> 
-                <span className='text-error text-sm'>Cons: {company?.featuredReview?.cons}</span> 
+                <span className='text-sm mr-2'>Pros: {company?.featuredReview?.pros}</span> 
+                <span className='text-sm'>Cons: {company?.featuredReview?.cons}</span> 
             </div>
         </div>
     </div>
