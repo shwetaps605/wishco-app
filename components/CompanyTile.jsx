48dd1ab41a1abb3602bba73f7f1aa1219256a8d2
Roy <shweroy@apac.corpdir.net>
2024-06-08 21:45:02 +0530

    add compnayTile component

---
+++ components/CompanyTile.jsx
@@ -0,0 +1,15 @@
+import { fetchCompanyDetails } from "../utils/actions";
+import * as dayjs from 'dayjs'
+var relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
+
+const CompanyTile = ({company}) => {
+    return(
+        <div className="px-5 py-3 border-2 border-base-100 shadow-md hover:shadow-lg rounded-lg hover:cursor-pointer">
+            <p className="text-secondary text-md">{company.name}</p>
+            <span className="text-xs mt-2 text-white opacity-20">wishlisted {dayjs(company.addedAt).fromNow()}</span>
+        </div>
+    )
+}
+
+export default CompanyTile;
