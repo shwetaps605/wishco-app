7257048bc20884bc094cab5cd38f4a01ab366fc7
Roy <shweroy@apac.corpdir.net>
2024-06-26 18:13:09 +0530

    added footer

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,13 +1,16 @@
 
 import Header from '../../components/Header'
+import Footer from "../../components/Footer"
 
 const layout = ({children}) => {
     return (
-        <div className='bg-base-200 min-h-screen'>
+        <div className='bg-base-200 flex flex-col min-h-[100vh]'>
         <Header/>
-        <div className='px-20 py-8'>
+        
+        <div className='px-20 py-8 flex-grow '>
             {children}
         </div>
+        <Footer/>
         </div>
         
     );
---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return(
+        <div className="bg-base-300 w-full py-3 px-10 bg-opacity-85 absolute bottom-0">
+           <p className="text-xs opacity-40 text-center mx-auto">Made by love, fueled by coffee!</p>
+        </div>
+    )
+}
+
+export default Footer;
