761cab691ede7ddbb58d7570cffeb2dde7bd315b
SHWEROY_T3DT <shweroy@apac.corpdir.net>
2024-08-10 23:18:32 +0530

    load companies for user in the main view

--- app/company/page.js
+++ app/company/page.js
@@ -1,6 +1,10 @@
+'use client'
+import { useUser } from "@clerk/nextjs"
 import CompanyTile from "../../components/CompanyTile"
 import GridLayout from "../../layouts/GridLayout"
 import AddCompany from "../../components/AddCompany"
+import { useQuery } from "@tanstack/react-query"
+import { findUser } from "../../utils/actions"
 
 const companyMock = [
     {
@@ -33,13 +37,50 @@ const companyMock = [
 
 
 const CompanyPage = () => {
+
+    const userQuery = useUser();
+    console.log("Signed in user:", userQuery.user)
+    console.log('USER ID--->',userQuery.user?.id)
+
+    const { data, isPending , isError} = useQuery({
+        queryKey: ['user', userQuery.user?.id],
+        queryFn: () => findUser(userQuery.user?.id),
+        enabled: userQuery.isLoaded
+    });
+
+    let renderedContent;
+    renderedContent = companyMock.map(company => {
+        return <CompanyTile company={company} key={company.name}/>  
+})
+
+    
+
+    if(isPending) {
+        renderedContent = <div>
+            <h1 className="text-xl text-primary m-auto">Loading Companies For user</h1>
+        </div>
+    }
+
+    if(isError|| data.data.companies !== undefined || data.data.companies?.length === 0 ) {
+        renderedContent = <div>
+            <h1 className="text-xl text-secondary m-auto">You have not wishlisted any companies yet.</h1>
+        </div>
+    }
+
+    if(data.data?.companies?.length > 0 ) {
+        const companies = data.data?.companies;
+        console.log("COMPANIES ADDED FOR USER:-->", companies)
+        renderedContent = companies.map(company => {
+                return <CompanyTile company={company} key={company.name}/>
+        })
+    }
+
+
     return(
         <div>
             <AddCompany/>
             <GridLayout>
-                {companyMock.map(company => {
-                    return <CompanyTile company={company} key={company.name}/>
-                })}
+               {renderedContent}
             </GridLayout>
 
         </div>
